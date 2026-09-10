923e8924d080a3825f23058787902d671b508d9a
dawg-codes <sumanthpc@yahoo.co.in>
2026-09-10 18:22:47 +0530

    Remove SMSInboxReader plugin from config
    Removed SMSInboxReader plugin configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,14 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dawgcode.expensetracker',
   appName: 'Expense Tracker Lite',
-  webDir: 'dist',
-  plugins: {
-    SMSInboxReader: {
-      android: {
-        name: 'com.capacitor.sms.reader.SMSInboxReaderPlugin'
-      }
-    }
-  }
+  webDir: 'dist'
 };
 
 export default config;
